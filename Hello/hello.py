ebbddf3b89df7d168f0171b3b42e310895f9be10
careychua <careychua@bikerider.com>
2020-05-28 15:02:44 +0800

    Initial commit

---
+++ Hello/hello.py
@@ -0,0 +1,11 @@
+# get method get_string() from module cs50
+from cs50 import get_string
+
+
+def main():
+    print("Hello,", get_string("What is your name? "))
+
+
+# to run main()
+if __name__ == "__main__":
+    main()
